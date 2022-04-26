0ae13beeb3d32725fff9e490cb650b1bc7daa08e
Godmode59 <jasonkim226@gmail.com>
2022-04-26 15:41:36 +0900

    8주차

---
+++ PP05/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+
+T sum(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	vector<int> a;
+
+	cout << sum<double>(3.9, 4.9) << endl;
+
+	cout << a.size() << endl;
+
+	a.push_back(1);
+
+	cout << a.size() << endl;
+	cout << "°ª: " << a[0] << endl;
+
+
+	return 0;
+}
